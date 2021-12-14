193623149fb1273877f0572d36facc4c206e9f3c
Bryan Luis <baluist@correo.udistrital.edu.co>
2021-12-14 18:00:22 -0500

    Se configuran rutas

--- apps/shell/src/app/app.module.ts
+++ apps/shell/src/app/app.module.ts
@@ -1,13 +1,22 @@
-import { NgModule } from '@angular/core';
+import { APP_BASE_HREF, HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  providers: [
+    { provide: APP_BASE_HREF, useValue: '/restaurante/' },
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
+    { provide: LOCALE_ID, useValue: 'es-Co' },
+  ],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
